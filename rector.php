8382eba79e9509fbc4ba27f2c1c3963a97df72b1
Dan Harrin <git@danharrin.com>
2025-02-04 10:43:17 +0000

    Update rector.php

--- rector.php
+++ rector.php
@@ -1,14 +1,10 @@
 <?php
 
-use Filament\Upgrade\Rector\RenameSchemaParamToMatchTypeRector;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__ . '/packages',
-    ])
-    ->withRules([
-        RenameSchemaParamToMatchTypeRector::class,
     ])
     ->withTypeCoverageLevel(0)
     ->withDeadCodeLevel(0)
