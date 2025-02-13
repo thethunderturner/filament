9a8e54bd84d52a91a7e210f1d56a2ebb9b06e7a9
Dan Harrin <git@danharrin.com>
2025-02-13 21:23:35 +0000

    refactor section

--- packages/schemas/resources/views/components/section.blade.php
+++ packages/schemas/resources/views/components/section.blade.php
@@ -1,61 +1,75 @@
 @php
+    $afterHeader = $getChildComponentContainer($schemaComponent::AFTER_HEADER_CONTAINER)?->toHtmlString();
+    $extraAttributes = $getExtraAttributes();
+    $extraAlpineAttributes = $getExtraAlpineAttributes();
+    $id = $getId();
     $isAside = $isAside();
+    $isCollapsed = $isCollapsed();
+    $isCollapsible = $isCollapsible();
+    $isCompact = $isCompact();
+    $isContained = $isContained();
     $isDivided = $isDivided();
+    $isFormBefore = $isFormBefore();
+    $description = $getDescription();
+    $footer = $getChildComponentContainer($schemaComponent::FOOTER_CONTAINER)?->toHtmlString();
+    $heading = $getHeading();
+    $headingTag = $getHeadingTag();
+    $icon = $getIcon();
+    $iconColor = $getIconColor();
+    $iconSize = $getIconSize();
+    $shouldPersistCollapsed = $shouldPersistCollapsed();
+    $isSecondary = $isSecondary();
 @endphp
 
-@if (filled($label = $getLabel()))
-    <div class="mb-2 flex items-center gap-x-3">
-        {{ $getChildComponentContainer($schemaComponent::BEFORE_LABEL_CONTAINER) }}
+<div class="fi-sc-section">
+    @if (filled($label = $getLabel()))
+        <div class="fi-sc-section-label-ctn">
+            {{ $getChildComponentContainer($schemaComponent::BEFORE_LABEL_CONTAINER) }}
 
-        <div
-            class="text-sm leading-6 font-medium text-gray-950 dark:text-white"
-        >
-            {{ $label }}
+            <div class="fi-sc-section-label">
+                {{ $label }}
+            </div>
+
+            {{ $getChildComponentContainer($schemaComponent::AFTER_LABEL_CONTAINER) }}
         </div>
+    @endif
 
-        {{ $getChildComponentContainer($schemaComponent::AFTER_LABEL_CONTAINER) }}
-    </div>
-@endif
-
-@if ($aboveContentContainer = $getChildComponentContainer($schemaComponent::ABOVE_CONTENT_CONTAINER)?->toHtmlString())
-    <div class="mb-2">
+    @if ($aboveContentContainer = $getChildComponentContainer($schemaComponent::ABOVE_CONTENT_CONTAINER)?->toHtmlString())
         {{ $aboveContentContainer }}
-    </div>
-@endif
+    @endif
 
-<x-filament::section
-    :after-header="$getChildComponentContainer($schemaComponent::AFTER_HEADER_CONTAINER)?->toHtmlString()"
-    :aside="$isAside"
-    :collapsed="$isCollapsed()"
-    :collapsible="$isCollapsible() && (! $isAside)"
-    :compact="$isCompact()"
-    :contained="$isContained()"
-    :content-before="$isFormBefore()"
-    :description="$getDescription()"
-    :divided="$isDivided"
-    :footer="$getChildComponentContainer($schemaComponent::FOOTER_CONTAINER)?->toHtmlString()"
-    :has-content-el="false"
-    :heading="$getHeading()"
-    :heading-tag="$getHeadingTag()"
-    :icon="$getIcon()"
-    :icon-color="$getIconColor()"
-    :icon-size="$getIconSize()"
-    :persist-collapsed="$shouldPersistCollapsed()"
-    :secondary="$isSecondary()"
-    :attributes="
-        \Filament\Support\prepare_inherited_attributes($attributes)
-            ->merge([
-                'id' => $getId(),
-            ], escape: false)
-            ->merge($getExtraAttributes(), escape: false)
-            ->merge($getExtraAlpineAttributes(), escape: false)
-    "
->
-    {{ $getChildComponentContainer()->gap(! $isDivided)->extraAttributes(['class' => 'fi-section-content']) }}
-</x-filament::section>
+    <x-filament::section
+        :after-header="$afterHeader"
+        :aside="$isAside"
+        :collapsed="$isCollapsed"
+        :collapsible="$isCollapsible && (! $isAside)"
+        :compact="$isCompact"
+        :contained="$isContained"
+        :content-before="$isFormBefore"
+        :description="$description"
+        :divided="$isDivided"
+        :footer="$footer"
+        :has-content-el="false"
+        :heading="$heading"
+        :heading-tag="$headingTag"
+        :icon="$icon"
+        :icon-color="$iconColor"
+        :icon-size="$iconSize"
+        :persist-collapsed="$shouldPersistCollapsed"
+        :secondary="$isSecondary"
+        :attributes="
+            \Filament\Support\prepare_inherited_attributes($attributes)
+                ->merge([
+                    'id' => $id,
+                ], escape: false)
+                ->merge($extraAttributes, escape: false)
+                ->merge($extraAlpineAttributes, escape: false)
+        "
+    >
+        {{ $getChildComponentContainer()->gap(! $isDivided)->extraAttributes(['class' => 'fi-section-content']) }}
+    </x-filament::section>
 
-@if ($belowContentContainer = $getChildComponentContainer($schemaComponent::BELOW_CONTENT_CONTAINER)?->toHtmlString())
-    <div class="mt-2">
+    @if ($belowContentContainer = $getChildComponentContainer($schemaComponent::BELOW_CONTENT_CONTAINER)?->toHtmlString())
         {{ $belowContentContainer }}
-    </div>
-@endif
+    @endif
+</div>
