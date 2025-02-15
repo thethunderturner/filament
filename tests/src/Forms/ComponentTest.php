6a25dffe581f1244ee7bba5ec6ae99900b814e96
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2025-02-15 17:05:59 +0100

    WIP

--- tests/src/Forms/ComponentTest.php
+++ tests/src/Forms/ComponentTest.php
@@ -70,26 +70,26 @@ it('can have meta', function () {
 });
 
 it('can clone', function () {
-	$cloneCallbackCount = 0;
-	$cloneCallbackClone = null;
-	$cloneCallbackOriginal = null;
-	
-	$component = (new Component)
-		->afterClone(function (Component $clone, Component $original) use (&$cloneCallbackCount, &$cloneCallbackClone, &$cloneCallbackOriginal) {
-			$cloneCallbackCount++;
-			$cloneCallbackClone = $clone;
-			$cloneCallbackOriginal = $original;
-		});
-	
-	$clone = $component->getClone();
-	
-	expect($cloneCallbackCount)
-		->toBe(1);
-	
-	expect($cloneCallbackClone)
-		->not->toBe($component)
-		->toBe($clone);
-	
-	expect($cloneCallbackOriginal)
-		->toBe($component);
+    $cloneCallbackCount = 0;
+    $cloneCallbackClone = null;
+    $cloneCallbackOriginal = null;
+
+    $component = (new Component)
+        ->afterClone(function (Component $clone, Component $original) use (&$cloneCallbackCount, &$cloneCallbackClone, &$cloneCallbackOriginal) {
+            $cloneCallbackCount++;
+            $cloneCallbackClone = $clone;
+            $cloneCallbackOriginal = $original;
+        });
+
+    $clone = $component->getClone();
+
+    expect($cloneCallbackCount)
+        ->toBe(1);
+
+    expect($cloneCallbackClone)
+        ->not->toBe($component)
+        ->toBe($clone);
+
+    expect($cloneCallbackOriginal)
+        ->toBe($component);
 });
